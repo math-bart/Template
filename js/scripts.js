5318effcbe8c7549d7a17dc05fec75f3af391552
Bartosz Wojtaś <el.klides@op.pl>
2018-05-30 19:43:48 +0200

    Add scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+$(function(){
+  $('.search-1').click(function() {
+    $('#photo1').css('animation-name', 'mojaAnimacja1');
+    $('#photo1').css('animation-duration', '6s');
+  });
+  $('.search-2').click(function() {
+    $('#photo2').css('animation-name', 'mojaAnimacja');
+    $('#photo2').css('animation-duration', '6s');
+  });
+  $('.search-3').click(function() {
+    $('#photo3').css('animation-name', 'mojaAnimacja');
+    $('#photo3').css('animation-duration', '6s');
+  });
+  $('.search-4').click(function() {
+    $('#photo4').css('animation-name', 'mojaAnimacja');
+    $('#photo4').css('animation-duration', '6s');
+  });
+  $('.search-6').click(function() {
+    $('#photo6').css('animation-name', 'mojaAnimacja2');
+    $('#photo6').css('animation-duration', '6s');
+  });
+  $('.search-7').click(function() {
+    $('#photo7').css('animation-name', 'mojaAnimacja1');
+    $('#photo7').css('animation-duration', '6s');
+  });
+  $('.search-8').click(function() {
+    $('#photo8').css('animation-name', 'mojaAnimacja');
+    $('#photo8').css('animation-duration', '6s');
+  });
+  $('.search-9').click(function() {
+    $('#photo9').css('animation-name', 'mojaAnimacja');
+    $('#photo9').css('animation-duration', '6s');
+  });
+  $('.search-10').click(function() {
+    $('#photo10').css('animation-name', 'mojaAnimacja');
+    $('#photo10').css('animation-duration', '6s');
+  });
+  $('.search-12').click(function() {
+    $('#photo12').css('animation-name', 'mojaAnimacja2');
+    $('#photo12').css('animation-duration', '6s');
+  });
+});
